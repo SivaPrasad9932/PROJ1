d622bffa65ec7e6e96f86935226e80d4b1f03345
SivaPrasad9932 <sivaprasad9932@gmail.com>
2026-06-14 15:19:36 +0530

    TL Created project

---
+++ Project1/src/main/java/com/siva/Project1/App.java
@@ -0,0 +1,13 @@
+package com.siva.Project1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
